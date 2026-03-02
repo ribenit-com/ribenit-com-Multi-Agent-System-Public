e304dd498b1ee0b1d86d19f179b0db01d3992ef9
ribenit-com <zuandilong@gmail.com>
2026-03-02 11:33:03 +0900

    Update git_constants.sh

--- GitOpsScript/config/git_constants.sh
+++ GitOpsScript/config/git_constants.sh
@@ -1,4 +1,4 @@
 GITLAB_USER="zuandilong@gmail.com"
-GITLAB_PAT="你的PAT"
+GITLAB_PAT="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"
 REPO_URL="https://github.com/ribenit-com/ribenit-com-Multi-Agent-System-Public.git"
 BRANCH="main"
